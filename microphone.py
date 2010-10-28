0936e08f8ceb4ec49bc967e9ea10c5c80670f8ad
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-10-28 15:11:37 +1100

    3d graph

--- microphone.py
+++ microphone.py
@@ -14,7 +14,7 @@ class Microphone(pyglet.window.Window):
 
     def __init__(self, *args, **kw):
         super(Microphone, self).__init__(*args, **kw)
-        self.num_samples = 512
+        self.num_samples = 256
         self.frames_per_buffer = self.num_samples
         self.format = pyaudio.paInt16
         self.channels = 1
@@ -30,8 +30,10 @@ class Microphone(pyglet.window.Window):
             frames_per_buffer=self.frames_per_buffer)
 
         self.buffer = ''
-
-        pyglet.clock.schedule_interval(self.update, 0.001)
+        self.buffer_w, self.buffer_h = self.width - 1, self.height - 1
+        self.buffer_size = self.buffer_w * self.buffer_h
+        self.screen_buffer = (GLuint * self.buffer_size)(0)
+        pyglet.clock.schedule_interval(self.update, 0.01)
 
     def read_fft(self):
         try:
@@ -43,32 +45,36 @@ class Microphone(pyglet.window.Window):
         return fft(normalized_data)[1:1 + self.num_samples // 2]
 
     def update(self, m):
-        data = self.read_fft()
-        if data is None:
-            return
-        
         self.clear()
 
-        if 0:
-            data *= 100
-            data += self.width / 2
-            pyglet.graphics.draw(len(data) // 2, pyglet.gl.GL_LINE_LOOP,
-                ('v2f', data)
-            )
+        data = None
+        while data is None:
+            data = self.read_fft()
+        
+        glDisable(GL_BLEND)
+        glRasterPos2i(1, 2)
+        glDrawPixels(self.buffer_w, self.buffer_h, GL_RGBA, GL_BYTE, 
+            self.screen_buffer)
 
-        else:
-            graph = []
-            w = 5
-            glLineWidth(w)
-            for s, value in enumerate(data):
-                graph.append(s * w + w / 2)
-                graph.append(0)
-                graph.append(s * w + w / 2)
-                graph.append(value * 100)
-            pyglet.graphics.draw(len(graph) // 2, pyglet.gl.GL_LINES,
-                ('v2f', graph))
+        glColor4f(1, 1, 1, 0.5)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        
+        graph = []
+        w = 5
+        glLineWidth(w)
+        for s, value in enumerate(data):
+            graph.append(s * w + w / 2)
+            graph.append(0)
+            graph.append(s * w + w / 2)
+            graph.append(value * 100000 / self.num_samples)
+        pyglet.graphics.draw(len(graph) // 2, pyglet.gl.GL_LINES,
+            ('v2f', graph))
+        
+        glReadPixels(0, 0, self.buffer_w, self.buffer_h, GL_RGBA, GL_BYTE, 
+                self.screen_buffer)
 
 if __name__ == '__main__':
-    window = Microphone(width=2500, height=768)
+    window = Microphone(width=1024, height=768)
     pyglet.app.run()
 
