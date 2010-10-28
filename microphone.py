9c3e4821be21c91eb1bd7ed0085bfd02b3d8084b
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-10-28 13:55:48 +1100

    pyglet test

--- microphone.py
+++ microphone.py
@@ -5,40 +5,57 @@ import os
 import pyaudio
 import wave
 import sys
+import pyglet
 
-chunk = 1024
-FORMAT = pyaudio.paInt8
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 0.1
-WAVE_OUTPUT_FILENAME = "output.wav"
+class Microphone(pyglet.window.Window):
 
-p = pyaudio.PyAudio()
+    def __init__(self):
+        super(Microphone, self).__init__()
+        self.label = pyglet.text.Label('Hello, world!')
 
-stream = p.open(format = FORMAT,
-                channels = CHANNELS,
-                rate = RATE,
-                input = True,
-                frames_per_buffer = chunk)
+        self.chunk = 1024
+        FORMAT = pyaudio.paInt8
+        self.channels = 1
+        self.rate = 44100
 
-buffer = ''
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format = FORMAT,
+            channels = self.channels,
+            rate = self.rate,
+            input = True,
+            frames_per_buffer=self.chunk)
 
-while 1:
-    data = stream.read(chunk)
-    buffer += data
-    if len(buffer) <= RATE:
-        datachunk = buffer[:RATE]
-        buffer = buffer[RATE:]
-        # os.system('clear')
-        numbers = [ord(a) for a in datachunk]
-        vol = sum(numbers)
-        print '%10i %.2f' % (vol, vol / (RATE)),
-        print '-' * int(vol / RATE * 10)
+        self.buffer = ''
 
-#all = []
-#for i in range(0, RATE / chunk * RECORD_SECONDS):
-#    data = stream.read(chunk)
-#    all.append(data)
-#print "* done recording"
+        pyglet.clock.schedule_interval(self.update, 0.001)
 
+    def next_sample(self):
+        while 1:
+            try:
+                data = self.stream.read(self.chunk)
+            except IOError:
+                continue
+            self.buffer += data
+            if len(self.buffer) <= self.rate:
+                datachunk = self.buffer[:self.rate]
+                self.buffer = self.buffer[self.rate:]
+                numbers = [ord(a) for a in datachunk]
+                vol = sum(numbers)
+                print '%10i %.2f' % (vol, vol / (self.rate)),
+                print '-' * int(vol / self.rate * 10)
+                return numbers, vol
+
+    def update(self, m):
+        self.clear()
+        data, vol = self.next_sample()
+        self.label.text = str(vol)
+        self.label.draw()
+        pyglet.graphics.draw(int(len(data) / 2), pyglet.gl.GL_LINE_LOOP,
+            ('v2i', data)
+        )
+
+if __name__ == '__main__':
+    window = Microphone()
+    pyglet.app.run()
 
